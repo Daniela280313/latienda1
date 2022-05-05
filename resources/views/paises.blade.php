0feca0d7ea0465782cdad6088a2c84f8899308a2
Yonathan <Yonathan1847@hotmail.com>
2022-05-05 15:55:53 -0500

    evidencia 1

--- resources/views/paises.blade.php
+++ resources/views/paises.blade.php
@@ -12,34 +12,35 @@
     <table class="table table-bordered table-stripped">
         <theah>
             <tr>
-                <th>Nombre</th>
-                <th>Capital</th>
-                <th>Moneda</th>
-                <th>Poblacion</th>
-                <th>Ciudades</th>
+                <th  style="background-color:cyan">Nombre</th>
+                <th style="background-color:cyan">Capital</th>
+                <th style="background-color:cyan">Moneda</th>
+                <th style="background-color:cyan">Poblacion</th>
+                <th style="background-color:cyan">Ciudades</th>
             </tr>
         </theah>
         <tbody>
             @foreach($paises as $pais => $infopais)
             <tr>
-                <td rowspan='{{ count($infopais["ciudades"]) }}'>
+                <td style="background-color:orange" rowspan='{{ count($infopais["ciudades"]) }}'>
                     {{ $pais }}
                 </td>
-                <td  rowspan='{{ count($infopais["ciudades"]) }}'>
+                <td style="background-color:yellow" rowspan='{{ count($infopais["ciudades"]) }}'>
                     {{ $infopais["capital"] }}
                 </td>
-                <td  rowspan='{{ count($infopais["ciudades"]) }}'>
+                <td style="background-color:green"  rowspan='{{ count($infopais["ciudades"]) }}'>
                     {{ $infopais["moneda"] }}
                 </td>
-                <td  rowspan='{{ count($infopais["ciudades"]) }}'>
+                <td  style="background-color:red"  rowspan='{{ count($infopais["ciudades"]) }}'>
                     {{ $infopais["poblacion"] }}
                 </td>
                 @foreach($infopais["ciudades"] as $ciudad )
                 <th> 
                     {{  $ciudad  }}
                 </th>
-                </th> 
+                </tr> 
                 @endforeach
+
             @endforeach
         </tbody>
         <tfoot></tfoot>
